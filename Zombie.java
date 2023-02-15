4f66bd9c3b1d7cad7c6c18d7649409a6fdf8c3d9
liyuxiao222 <117941566+liyuxiao222@users.noreply.github.com>
2023-02-14 20:31:58 -0500

    a

---
+++ Zombie.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class Zombie extends Actor
+{
+    
+    
+    public Zombie(){
+        
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
